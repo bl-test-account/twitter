c433d5ae24232765ade8895c41bbf5aa0c3172dc
bl-test-account <plugin.developer@bytelight.com>
2013-02-06 15:51:10 -0800

    Saved at Wed Feb 06 2013 18:49:47 GMT-0500 (EST)

---
+++ show/page.js
@@ -0,0 +1,6 @@
+$(function() {
+  $('p').text('Hello World: this is the show page for displaying ' +
+              'any individual pieces of content that are added ' +
+              'to the timeline by your plugin.  NOTE: not all ' +
+              'plugins will have a show page');
+})

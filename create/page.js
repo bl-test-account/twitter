89a31fcd8eda7f728a836c6757a11cf3de6d2b55
bl-test-account <plugin.developer@bytelight.com>
2013-02-06 11:58:45 -0800

    Initial commit

---
+++ create/page.js
@@ -0,0 +1,17 @@
+$(function() {
+  
+  $('button').on('click', function() {
+    errors = BL.validateInput($('#tweetText'))
+    if (errors) {
+      return;
+    }
+    
+    json = {
+      tweetText: $('#tweetText').val(),
+      oncePerSession: $('#oncePerSession').is(':checked')
+    }
+    
+    BL.createContent(JSON.stringify(json))
+  })
+  
+})
